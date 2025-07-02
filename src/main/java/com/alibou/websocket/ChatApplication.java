eff00e8d63ebf78d5da157f34d629a98b5301a27
Sohan Bhattacharya <155570129+Curious-Buddy777@users.noreply.github.com>
2025-07-02 17:29:29 +0530

    ChatApplication.java

--- src/main/java/com/alibou/websocket/ChatApplication.java
+++ src/main/java/com/alibou/websocket/ChatApplication.java
@@ -3,27 +3,11 @@ package com.alibou.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @SpringBootApplication
 public class ChatApplication {
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ChatApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
 
-        // Bind to Railway's dynamic port
-        Map<String, Object> props = new HashMap<>();
-        String port = System.getenv("PORT");
-        if (port != null) {
-            props.put("server.port", port);
-            System.out.println("✅ Binding to Railway dynamic port: " + port);
-        } else {
-            props.put("server.port", "8080");
-            System.out.println("⚠️ No PORT env var found. Falling back to 8080.");
-        }
-
-        app.setDefaultProperties(props);
-        app.run(args);
-    }
 }
